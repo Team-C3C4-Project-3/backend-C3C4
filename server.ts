e1b319022fa7d6713e8eb9c707c52821bcf49360
Jenna <jr13114@my.bristol.ac.uk>
2022-01-05 14:46:53 +0100

    removed comment

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
 
-config(); //Read .env file lines as though they were env vars.
+config(); 
 
 //Call this script with the environment variable LOCAL set if you want to connect to a local db (i.e. without SSL)
 //Do not set the environment variable LOCAL if you want to connect to a heroku DB.
